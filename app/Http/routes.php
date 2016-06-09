14ae8dee6f14cd50f6739a797af893c76b699cb7
James Mwangi <karokijames40@gmail.com>
2016-06-09 18:05:12 +0300

    Change routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,8 +31,6 @@ Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
     Route::get('/home', 'HomeController@index');
-    Route::get('/accounts/fixed/account', ['as' => 'getFixedAccount', 'uses' => 'Account\AccountController@getFixed']);
-
 });
 
 Route::group(['middleware' => 'web', 'prefix' => 'accounts', 'before' => 'csrf'], function() {
@@ -52,6 +50,7 @@ Route::group(['middleware' => 'web', 'prefix' => 'accounts', 'before' => 'csrf']
        Route::post('/deposit-current', ['as' => 'depositCurrent', 'uses' => 'Account\AccountController@depositCurrent']);
 
        Route::get('/savings/account', ['as' => 'getSavingsAccount', 'uses' => 'Account\AccountController@getSavings']);
+       Route::get('/fixed/account/account', ['as' => 'getFixedAccount', 'uses' => 'Account\AccountController@getFixed']);
        Route::post('/fixed/save', ['as' => 'depositFixed', 'uses' => 'Account\AccountController@depositFixed']);
        Route::post('/savings/save', ['as' => 'saveSavings', 'uses' => 'Account\AccountController@depositSavings']);
        Route::get('/get/all', ['as' => 'allAccounts', 'uses' => 'Account\AccountController@getAll']);
