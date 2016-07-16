9b5b10e197037ce11ca756c39f694185615a191a
James Mwangi <karokijames40@gmail.com>
2016-07-16 21:23:07 +0300

    Remove noisy message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,7 +101,7 @@
     <script>
         $('.tag_list').select2({
             tags: true,
-            placeholder: 'You can select multiple users to transfer money to here',
+            placeholder: 'Select users',
             maximumSelectionLength: 10
         });
     </script>
