84b81cdc685dfa680f81e141a6935db0af5c99cd
James Mwangi <karokijames40@gmail.com>
2016-07-19 15:03:26 +0300

    Change app name to HBnk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>HBnk</title>
 
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
@@ -43,7 +43,7 @@
                 @if(\Auth::guest())
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Benki
+                    HBnk
                 </a>
                 @endif
             </div>
